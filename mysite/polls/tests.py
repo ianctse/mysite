97d8f4be9d24fadff885748d3ff37eb71e5a42ba
Ian Tse <ian.c.tse@gmail.com>
2019-02-09 18:47:36 -0800

    Fixed typo bug in function argument

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -33,7 +33,7 @@ class QuestionModelTests(TestCase):
         was_published_recently() returns True for questions whose pub_date
         is within the last day.
         """
-        time = timezone.now() - datetime.timedelta(hours=23, minute=59, seconds=59)
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
         recent_question = Question(pub_date=time)
 
         self.assertIs(recent_question.was_published_recently(), True)
